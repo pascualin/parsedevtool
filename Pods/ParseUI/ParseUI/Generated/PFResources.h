c5779e3d32900d4fa1ed5bd9229685ffc66a9cf2
Takeshi Hui <takeshi@niceway.org>
2015-06-09 20:04:28 -0700

    using NWNav

--- Pods/ParseUI/ParseUI/Generated/PFResources.h
+++ Pods/ParseUI/ParseUI/Generated/PFResources.h
@@ -1,13 +1,13 @@
 // This is an auto-generated file.
 #import <Foundation/Foundation.h>
 @interface PFResources : NSObject
-+ (NSData *)facebook_icon_png;//modified:2015-06-08 22:51:38 +0800
-+ (NSData *)facebook_icon2x_png;//modified:2015-06-08 22:51:38 +0800
-+ (NSData *)facebook_icon3x_png;//modified:2015-06-08 22:51:38 +0800
-+ (NSData *)parse_logo_png;//modified:2015-06-08 22:51:38 +0800
-+ (NSData *)parse_logo2x_png;//modified:2015-06-08 22:51:38 +0800
-+ (NSData *)parse_logo3x_png;//modified:2015-06-08 22:51:38 +0800
-+ (NSData *)twitter_icon_png;//modified:2015-06-08 22:51:38 +0800
-+ (NSData *)twitter_icon2x_png;//modified:2015-06-08 22:51:38 +0800
-+ (NSData *)twitter_icon3x_png;//modified:2015-06-08 22:51:38 +0800
++ (NSData *)facebook_icon_png;//modified:2015-06-09 19:46:47 -0700
++ (NSData *)facebook_icon2x_png;//modified:2015-06-09 19:46:47 -0700
++ (NSData *)facebook_icon3x_png;//modified:2015-06-09 19:46:47 -0700
++ (NSData *)parse_logo_png;//modified:2015-06-09 19:46:47 -0700
++ (NSData *)parse_logo2x_png;//modified:2015-06-09 19:46:47 -0700
++ (NSData *)parse_logo3x_png;//modified:2015-06-09 19:46:47 -0700
++ (NSData *)twitter_icon_png;//modified:2015-06-09 19:46:47 -0700
++ (NSData *)twitter_icon2x_png;//modified:2015-06-09 19:46:47 -0700
++ (NSData *)twitter_icon3x_png;//modified:2015-06-09 19:46:47 -0700
 @end
